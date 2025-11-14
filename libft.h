5a9ad9864069039d19b28055f78b4df5aeec115d
Valeriia Bokotejova <vbokotej@c2r2s1.42prague.com>
2025-11-14 23:31:47 +0100

    add prototypes for future functions to libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vbokotej <vbokotej@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 22:50:26 by vbokotej          #+#    #+#             */
-/*   Updated: 2025/11/14 23:08:18 by vbokotej         ###   ########.fr       */
+/*   Updated: 2025/11/14 23:28:57 by vbokotej         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,23 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
+
+void	*memset(void *s, int c, size_t n);
+void	bzero(void *s, size_t n);
+void	*memcpy(void *dest, const void *src, size_t n);
+void	*memmove(void *dest, const void *src, size_t n);
+size_t	strlcpy(char *dst, const char *src, size_t size);
+size_t	strlcat(char *dst, const char *src, size_t size);
+int		toupper(int c);
+int		tolower(int c);
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
+int		strncmp(const char *s1, const char *s2, size_t n);
+void	*memchr(const void *s, int c, size_t n);
+int		memcmp(const void *s1, const void *s2, size_t n);
+char	*strnstr(const char *big, const char *little, size_t len);
+int		atoi(const char *nptr);
+void	*calloc(size_t nmemb, size_t size);
+char	*strdup(const char *s);
 
 #endif
