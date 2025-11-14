ead68c87bf25d4c661e82381c355cd039af3ab24
Valeriia Bokotejova <vbokotej@c2r2s1.42prague.com>
2025-11-14 23:11:57 +0100

    add libft.h header file

--- libft.h
+++ libft.h
@@ -1,23 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vbokotej <vbokotej@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/14 22:14:16 by vbokotej          #+#    #+#             */
-/*   Updated: 2025/11/14 23:07:36 by vbokotej         ###   ########.fr       */
+/*   Created: 2025/11/14 22:50:26 by vbokotej          #+#    #+#             */
+/*   Updated: 2025/11/14 23:08:18 by vbokotej         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef LIBFT_H
+# define LIBFT_H
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+
+#endif
