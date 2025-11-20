e4821aeed7be6d4d1b183c2a577ab95a9206ae0a
Valeria <valeria.bokotejova@gmail.com>
2025-11-20 21:20:28 +0100

    update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vbokotej <vbokotej@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 22:50:26 by vbokotej          #+#    #+#             */
-/*   Updated: 2025/11/14 23:28:57 by vbokotej         ###   ########.fr       */
+/*   Updated: 2025/11/20 21:18:29 by vbokotej         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,22 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
-
-void	*memset(void *s, int c, size_t n);
-void	bzero(void *s, size_t n);
-void	*memcpy(void *dest, const void *src, size_t n);
-void	*memmove(void *dest, const void *src, size_t n);
-size_t	strlcpy(char *dst, const char *src, size_t size);
-size_t	strlcat(char *dst, const char *src, size_t size);
-int		toupper(int c);
-int		tolower(int c);
-char	*strchr(const char *s, int c);
-char	*strrchr(const char *s, int c);
-int		strncmp(const char *s1, const char *s2, size_t n);
-void	*memchr(const void *s, int c, size_t n);
-int		memcmp(const void *s1, const void *s2, size_t n);
-char	*strnstr(const char *big, const char *little, size_t len);
-int		atoi(const char *nptr);
-void	*calloc(size_t nmemb, size_t size);
-char	*strdup(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 
 #endif
