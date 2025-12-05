5a8684980ffa3db842d1f77b8e8a3f5143dc7cf6
Valeria <valeria.bokotejova@gmail.com>
2025-12-06 00:31:42 +0100

    add ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbokotej <vbokotej@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 23:53:21 by vbokotej          #+#    #+#             */
+/*   Updated: 2025/12/06 00:28:36 by vbokotej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*source;
+	char	*dst;
+
+	source = (char *)src;
+	dst = (char *)dest;
+	while (n-- > 0)
+		*dst++ = *source++;
+	return (dest);
+}
+/*
+Copies n bytes from the source memory to the destination memory, one by one.
+Returns the original pointer to the destination
+*/
