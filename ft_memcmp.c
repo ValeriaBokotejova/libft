013367b0bf31cf78ed08fdc12814616f7e7df1c0
Valeria <valeria.bokotejova@gmail.com>
2025-12-06 02:10:38 +0100

    add ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbokotej <vbokotej@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/06 01:20:38 by vbokotej          #+#    #+#             */
+/*   Updated: 2025/12/06 02:08:34 by vbokotej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (n-- > 0)
+	{
+		if (*str1 != *str2)
+			return ((int)(*str1 - *str2));
+		str1++;
+		str2++;
+	}
+	return (0);
+}
+/*
+Compares the first n bytes of s1 and s2.
+Returns 0 if equal, a positive or negative value if different
+*/
