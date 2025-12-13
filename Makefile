ffc478f751119775a2f83eedf6602e7fc8d20adb
Valeria <valeria.bokotejova@gmail.com>
2025-12-13 15:22:39 +0100

    add strtrim

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: vbokotej <vbokotej@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/06 20:06:11 by vbokotej          #+#    #+#              #
-#    Updated: 2025/12/12 07:29:54 by vbokotej         ###   ########.fr        #
+#    Updated: 2025/12/12 08:59:17 by vbokotej         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -41,7 +41,8 @@ ft_strrchr.c \
 ft_tolower.c \
 ft_toupper.c \
 ft_substr.c \
-ft_strjoin.c
+ft_strjoin.c \
+ft_strtrim.c
 
 OBJ = $(SRC_FILES:.c=.o)
 
---
+++ ft_strtrim.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbokotej <vbokotej@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/06 19:30:30 by vbokotej          #+#    #+#             */
+/*   Updated: 2025/12/12 08:58:33 by vbokotej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_is_in_set(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+	int		start;
+	int		end;
+	int		len;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_is_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end >= start && ft_is_in_set(s1[end], set))
+		end--;
+	len = end - start + 1;
+	trimmed = malloc(sizeof(char) * (len + 1));
+	if (!trimmed)
+		return (NULL);
+	ft_strlcpy(trimmed, s1 + start, len + 1);
+	return (trimmed);
+}
+/*
+Creates a new string from s1,
+	removing any characters at the start and end that are in set.
+Returns the new trimmed string, or NULL if memory allocation fails
+*/
